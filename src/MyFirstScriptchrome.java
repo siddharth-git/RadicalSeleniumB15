c3671ebb55099c48fdb92397c5d21283b94569cf
siddharth-git <sidshine007@gmail.com>
2018-07-09 17:55:13 +0530

    testGit

--- src/MyFirstScriptchrome.java
+++ src/MyFirstScriptchrome.java
@@ -2,7 +2,7 @@ package Basic;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;;
 
 public class MyFirstScriptchrome {
 
